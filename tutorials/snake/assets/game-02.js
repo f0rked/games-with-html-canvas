563a6378638b65256494aa5a6760acc67a547811
Héctor J. Hurtado Ruesga <hjhurtado@gmail.com>
2020-10-20 09:53:49 +0200

    Add code for part 2

---
+++ tutorials/snake/assets/game-02.js
@@ -0,0 +1,36 @@
+let canvasName = 'canvas',
+    canvas = null,
+    ctx = null;
+let x = 50,
+    y = 50;
+
+function paint(ctx) {
+  ctx.fillStyle = '#000000';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = '#00ff00';
+  ctx.fillRect(x, y, 10, 10);
+}
+
+function actions() {
+  x += 2;
+
+  if (x > canvas.width) {
+      x = 0;
+  }
+}
+
+function run() {
+  window.requestAnimationFrame(run);
+  actions();
+  paint(ctx);
+}
+
+function init() {
+  canvas = document.getElementById('canvas');
+  ctx = canvas.getContext('2d');
+
+  run();
+}
+
+ window.addEventListener('load', init, false);
