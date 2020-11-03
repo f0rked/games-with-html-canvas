880d2d114c250d735035abf640403463871314c1
Héctor J. Hurtado Ruesga <hjhurtado@gmail.com>
2020-11-03 22:34:34 +0100

    Add part 9

---
+++ tutorials/snake/assets/game-09.js
@@ -0,0 +1,383 @@
+/*jshint esversion: 5*/
+(function (window, document, undefined) {
+  "use strict";
+
+  const CANVAS_NAME = "canvas",
+        KEY_LEFT = "ArrowLeft",
+        KEY_UP = "ArrowUp",
+        KEY_RIGHT = "ArrowRight",
+        KEY_DOWN = "ArrowDown",
+        MOVING_UP = 0,
+        MOVING_RIGHT = 1,
+        MOVING_DOWN = 2,
+        MOVING_LEFT = 3,
+        KEY_ENTER = "Enter";
+  const ASSET_SNAKE_SECTION = "assets/sect.png",
+        ASSET_FOOD = "assets/apple.png",
+        ASSET_EAT = ["assets/eat.oga", "assets/eat.m4a"],
+        ASSET_OVER = ["assets/over.oga", "assets/over.m4a"];
+  const SCENE_MAIN_MENU = "MainMenu",
+        SCENE_GAME = "SnakeGame";
+
+  class Rectangle {
+    constructor(x, y, color, width, height) {
+      this.x = (x == null) ? 0 : x;
+      this.y = (y == null) ? 0 : y;
+      this.color = (color == null) ? '#ffffff' : color;
+      this.width = (width == null) ? 0 : width;
+      this.height = (height == null) ? this.width : height;
+    }
+
+    intersects(rect) {
+      if (rect == null) {
+        window.console.warn('Missing parameters on function intersects');
+      } else {
+        return (this.x < rect.x + rect.width &&
+          this.x + this.width > rect.x &&
+          this.y < rect.y + rect.height &&
+          this.y + this.height > rect.y);
+      }
+    }
+
+    draw(ctx) {
+      if (ctx == null) {
+        window.console.warn('Missing parameters on function draw');
+      } else {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+      }
+    }
+  }
+
+  class SpriteRectangle extends Rectangle {
+    constructor(x, y, color, image, width, height) {
+      super(x, y, color, width, height);
+      this.bkgImg = image
+    }
+
+    draw(ctx) {
+      if (ctx != null) {
+        if (this.bkgImg != null && this.bkgImg.width > 0)
+          ctx.drawImage(this.bkgImg, this.x, this.y)
+        else
+          super.draw(ctx)
+      }
+    }
+  }
+
+  class Food extends SpriteRectangle {
+    relocate(maxWidth, maxHeight) {
+      this.x = SnakeGame.random(maxWidth / this.width - 1) * this.width;
+      this.y = SnakeGame.random(maxHeight / this.height - 1) * this.height;
+    }
+  }
+
+  class Snake {
+    constructor(x, y, direction, width, height, color, sprite) {
+      this.width = (width) ? width : 10;
+      this.height = (height) ? height : 10;
+      this.color = (color) ? color : "#00ff00";
+      this.headSprite = this.bodySprite = sprite;
+      this.body = [];
+
+      this.reborn(x, y, direction)
+    }
+
+    reborn(x, y, direction) {
+      this.body.length = 0;
+
+      for (let i = 0; i < 3; i++) {
+        this.body.push(new SpriteRectangle(x, y, this.color, this.headSprite, this.width, this.height));
+        if (direction == MOVING_UP)
+          y -= this.height;
+        else if (direction == MOVING_DOWN)
+          y += this.height;
+        else if (direction == MOVING_LEFT)
+          x += this.width;
+        else if (direction == MOVING_RIGHT)
+          x -= this.width;
+      }
+    }
+
+    draw(ctx) {
+      for (let i = 0; i < this.body.length; i++) {
+        this.body[i].draw(ctx);
+      }
+    }
+
+    move(howMuch, direction, maxWidth, maxHeight) {
+      // Move snake's body
+      if (direction != null) {
+        for (let i = this.body.length - 1; i > 0; i--) {
+          this.body[i].x = this.body[i - 1].x;
+          this.body[i].y = this.body[i - 1].y;
+        }
+      }
+
+      // Move snake's head
+      switch (direction) {
+        case MOVING_UP: this.body[0].y -= howMuch; break;
+        case MOVING_RIGHT: this.body[0].x += howMuch; break;
+        case MOVING_DOWN: this.body[0].y += howMuch; break;
+        case MOVING_LEFT: this.body[0].x -= howMuch; break;
+      }
+
+      // Out screen
+      if (this.body[0].x < 0) {
+        this.body[0].x = maxWidth - howMuch;
+      } else if (this.body[0].x > maxWidth) {
+        this.body[0].x = 0;
+      }
+
+      if (this.body[0].y < 0) {
+        this.body[0].y = maxHeight - howMuch;
+      } else if (this.body[0].y > maxHeight) {
+        this.body[0].y = 0;
+      }
+    }
+
+    hasEaten(food) {
+      if (this.body[0].intersects(food)) {
+        this.body.push(new SpriteRectangle(0, 0, this.color, this.headSprite, this.width, this.height));
+        return true;
+      }
+      return false;
+    }
+
+    hasBitten() {
+      for (let i = this.body.length - 1; i > 1; i--) {
+        if (this.body[0].intersects(this.body[i])) {
+          return true;
+        }
+      }
+
+    }
+  }
+
+  class Scene {
+
+    constructor(game, sceneName) {
+      this.game = game;
+      Scene.Script[sceneName] = this;
+    }
+
+    // Initialize the scene variables needed to start the execution
+    load() { }
+    // Paint the elements of the scene using the provided graphics context
+    paint(ctx) { throw new Error("Pending implementation"); }
+    // Compute the next state for the game
+    actions() { throw new Error("Pending implementation"); }
+
+    static Current;
+    static Script = {};
+
+    static ChangeScene(scene) {
+      if (Scene.Script[scene]) {
+        Scene.Current = scene;
+        Scene.Script[scene].load();
+      } else
+        throw new Error("No scene " + scene + " was found");
+    }
+
+    static paint(ctx) {
+      if (Scene.Script[Scene.Current])
+        Scene.Script[Scene.Current].paint(ctx);
+    }
+
+    static actions() {
+      if (Scene.Script[Scene.Current])
+        Scene.Script[Scene.Current].actions();
+    }
+  }
+
+  class MainMenuScene extends Scene {
+    constructor(game) { super(game, SCENE_MAIN_MENU); }
+
+    paint(ctx) {
+      // Clean canvas
+      ctx.fillStyle = '#003300';
+      ctx.fillRect(0, 0, this.game.getWidth(), this.game.getHeight());
+
+      // Draw title
+      ctx.fillStyle = '#ffffff';
+      ctx.textAlign = 'center';
+      ctx.fillText('SNAKE', 150, 60);
+      ctx.fillText('Press Enter', 150, 90);
+    }
+
+    actions() {
+      // Load next scene
+      if (this.game.lastPressed === KEY_ENTER) {
+        Scene.ChangeScene(SCENE_GAME);
+        this.game.lastPressed = null;
+      }
+    }
+  }
+
+  class GameScene extends Scene {
+    constructor(game) {
+      super(game, SCENE_GAME);
+
+      this.movingDirection = MOVING_RIGHT;
+      this.pause = true;
+      this.score = 0;
+      this.gameover = false;
+
+      // Load assets
+      this.loadAssets();
+
+      // Create snake
+      this.snake = new Snake(300, 150, MOVING_RIGHT, 10, 10, '#00ff00', this.sectImg);
+
+      // Create initial food
+      this.food = new Food(450, 280, '#ff0000', this.foodImg, 10);
+    }
+
+    load() {
+      this.score = 0;
+      this.movingDirection = MOVING_RIGHT;
+
+      this.snake.reborn(300, 150, this.movingDirection);
+
+      this.food.relocate(this.game.getWidth(), this.game.getHeight());
+      this.gameover = false;
+    }
+
+    isPaused() { return this.pause ; }
+
+    isGameover() { return this.gameover ; }
+
+    loadAssets() {
+      let audioFormat = ((new Audio()).canPlayType('audio/ogg') != '') ? 0 : 1;
+
+      this.sectImg = new Image();
+      this.foodImg = new Image();
+      this.sectImg.src = ASSET_SNAKE_SECTION;
+      this.foodImg.src = ASSET_FOOD;
+
+      this.eat = new Audio(ASSET_EAT[audioFormat]);
+      this.over = new Audio(ASSET_OVER[audioFormat]);
+    }
+
+    paint(ctx) {
+      ctx.fillStyle = '#000000';
+      ctx.fillRect(0, 0, this.game.getWidth(), this.game.getHeight());
+
+      // Draw score
+      ctx.fillStyle = '#ffffff';
+      ctx.fillText('Score: ' + this.score, 0, 20);
+
+      // Draw snake
+      this.snake.draw(ctx);
+
+      this.food.draw(ctx);
+
+      // Draw pause or gameover caption
+      if (this.isPaused()) {
+          ctx.textAlign = 'center';
+          if (this.isGameover()) {
+              ctx.fillText('GAME OVER', this.game.getWidth() / 2, this.game.getHeight() / 2);
+          } else {
+              ctx.fillText('PAUSE', this.game.getWidth() / 2, this.game.getHeight() / 2);
+          }
+          ctx.textAlign = 'left';
+      }
+    }
+
+    actions() {
+      if (!this.isPaused()) {
+        // GameOver reset
+        if (this.isGameover()) {
+          Scene.ChangeScene(SCENE_MAIN_MENU);
+        }
+
+        // Check direction
+        switch (this.game.lastPressed) {
+          case KEY_LEFT: this.movingDirection = MOVING_LEFT; break;
+          case KEY_UP: this.movingDirection = MOVING_UP; break;
+          case KEY_RIGHT: this.movingDirection = MOVING_RIGHT; break;
+          case KEY_DOWN: this.movingDirection = MOVING_DOWN; break;
+        }
+
+        // Move snake
+        this.snake.move(10, this.movingDirection, this.game.getWidth(), this.game.getHeight());
+
+        // Body Intersects
+        if (this.snake.hasBitten()) {
+            this.over.play();
+            this.gameover = true;
+            this.pause = true;
+        }
+
+        // Food eaten
+        if (this.snake.hasEaten(this.food)) {
+          this.eat.play();
+          this.score += 1; // also score++ or score = score + 1
+          this.food.relocate(this.game.getWidth(), this.game.getHeight());
+        }
+      }
+
+      // Pause/Unpause
+      if (this.game.lastPressed == KEY_ENTER) {
+          this.pause = !this.pause;
+          this.game.lastPressed = null;
+      }
+    }
+  }
+
+  class SnakeGame {
+
+    constructor(canvas) {
+      this.canvas = canvas;
+      this.ctx = canvas.getContext('2d');
+      this.lastPressed = null;
+
+      new MainMenuScene(this),
+      new GameScene(this);
+
+      Scene.ChangeScene(SCENE_MAIN_MENU);
+    }
+
+    getWidth() { return this.canvas.width; }
+
+    getHeight() { return this.canvas.height; }
+
+    repaint() {
+      window.requestAnimationFrame(this.repaint.bind(this));
+      Scene.paint(this.ctx);
+    }
+
+    run() {
+      setTimeout(this.run.bind(this), 50);
+      Scene.actions();
+    }
+
+    keyHandler() {
+      if (this.lastPressed == KEY_LEFT && event.key == KEY_RIGHT) ;
+      else if (this.lastPressed == KEY_UP && event.key == KEY_DOWN) ;
+      else if (this.lastPressed == KEY_RIGHT && event.key == KEY_LEFT) ;
+      else if (this.lastPressed == KEY_DOWN && event.key == KEY_UP) ;
+      else
+        this.lastPressed = event.key;
+
+      if (this.lastPressed == KEY_LEFT || this.lastPressed == KEY_UP || this.lastPressed == KEY_RIGHT || this.lastPressed == KEY_DOWN)
+        event.preventDefault();
+    }
+
+    static initialize() {
+
+      window.snakeGame = new SnakeGame(document.getElementById(CANVAS_NAME));
+
+      window.addEventListener("keydown", window.snakeGame.keyHandler.bind(window.snakeGame), false);
+
+      window.snakeGame.run();
+      window.snakeGame.repaint();
+    }
+
+    static random(max) {
+      return Math.floor(Math.random() * max);
+    }
+  }
+
+  window.addEventListener("load", SnakeGame.initialize, false);
+}(window, document));
---
+++ tutorials/snake/part-09.md
@@ -0,0 +1,298 @@
+---
+  layout: tutorial
+  id: snake
+---
+
+## Escenas
+
+Ahora que ya tenemos un juego con toda su funcionalidad y el código bien estructurado ha llegado el momento de añadirle detalles
+que lo hagan más atractivo. En esta sección vamos a ver como añadirle *"escenas"*, distintas pantallas para mostrar contenido
+adicional al juego, como los créditos, ayuda,...
+
+Quizá la forma más sencilla de manejar escenas sea usar una variable donde recogemos la escena actual y una serie de condicionales
+en las funciones de pintado y gestión de acciones:
+
+``` javascript
+const SCENE_WELCOME = 0,
+      SCENE_GAME = 1;
+
+let currentScene = SCENE_WELCOME;
+
+...
+
+function actions() {
+  if (currentScene == SCENE_WELCOME) {
+    // Actions SCENE_WELCOME
+    // ...
+  } else if (currentScene == SCENE_GAME) {
+    // Actions SCENE_GAME
+    // ...
+  }
+}
+
+function paint(ctx) {
+  if (currentScene == SCENE_WELCOME) {
+    // Paint SCENE_WELCOME
+    // ...
+  } else if (currentScene == SCENE_GAME) {
+    // Paint SCENE_GAME
+    // ...
+  }
+}
+
+```
+
+En cada rama se incluiría el código correspondiente a cada escena y sólo habría que modificar el valor de `currentScene` para
+cambiar de escena. Esta aproximación funciona bien si el número de escenas es pequeño y la lógica asociada a cada escena también,
+pero cuando aumenta el número de escenas o se incrementa el tamaño del código asociado a cada una de ellas, pronto se vuelve
+inmanejable. ¿Que podemos hacer entonces...?
+
+Más objetos... Esta alternativa, aunque es un poco más compleja y difícil de explicar, a la larga es mucho más práctica y no añade
+excesiva complejidad.
+
+En primer lugar crearemos una nueva clase base, `Scene`, que define los métodos necesarios para hacer funcionar una escena y los
+atributos y métodos estáticos para poder gestionar las escenas de un juego:
+
+``` javascript
+class Scene {
+
+  constructor(game, sceneName) {
+    this.game = game;
+    Scene.Script[sceneName] = this;
+  }
+
+  // Initialize the scene variables needed to start the execution
+  load() { }
+  // Paint the elements of the scene using the provided graphics context
+  paint(ctx) { throw new Error("Pending implementation"); }
+  // Compute the next state for the game
+  actions() { throw new Error("Pending implementation"); }
+
+  static Current;
+  static Script = {};
+
+  static ChangeScene(scene) {
+    if (Scene.Script[scene]) {
+      Scene.Current = scene;
+      Scene.Script[scene].load();
+    } else
+      throw new Error("No scene " + scene + " was found");
+  }
+
+  static paint(ctx) {
+    if (Scene.Script[Scene.Current])
+      Scene.Script[Scene.Current].paint(ctx);
+  }
+
+  static actions() {
+    if (Scene.Script[Scene.Current])
+      Scene.Script[Scene.Current].actions();
+  }
+}
+```
+
+Como verás sólo hemos implementado el constructor y los métodos estáticos, mientras que los métodos de instancia están vacíos o
+lanzan una excepción. En programación orientada a objetos esto se llama *"clase abstracta"*, es decir una clase que no se puede
+instanciar, su propósito es definir un interfaz que debe ser implementado por las clases que heredan de ella, tan sólo unos pocos
+métodos están implementados, a veces, ninguno. El concepto de *"clase abstracta"* no existe en Javascript, así que esto es lo más
+parecido.
+
+El constructor recibe como parámetros una instancia del juego y el nombre de la escena, que usa para registrar la instancia de la
+clase que se está creando en el objeto `Script`, donde luego puede ser buscado por el método estático `ChangeScene` que invoca el
+método `load` de la escena seleccionada para prepararlo todo para la ejecución. Los métodos estáticos `paint` y `run` simplemente
+llaman al método homónimo de la escena actual tras comprobar que existe en el objeto `Script`.
+
+Todas las escenas de nuestro juego deben heredar de la clase `Scene` y proveer una implementación para, al menos, `paint` y `actions`.
+
+Por último debemos cambiar el código de los métodos `repaint` y `run` de la clase `SnakeGame` para que ejecuten los métodos
+correspondientes de la escena actual:
+
+``` javascript
+    repaint() {
+      window.requestAnimationFrame(this.repaint.bind(this));
+      Scene.paint(this.ctx);
+    }
+
+    run() {
+      setTimeout(this.run.bind(this), 50);
+      Scene.actions();
+    }
+```
+
+### Primeras escenas
+
+Ya lo tenemos todo listo, ahora podemos crear las escenas de nuestro juego. En esta sección, y para comenzar, vamos a crear una
+escena para el menú principal y otra para el juego.
+
+Primero definimos 2 nuevas constantes con los nombres de nuestras escenas:
+
+``` javascript
+const SCENE_MAIN_MENU = "MainMenu",
+      SCENE_GAME = "SnakeGame";
+```
+
+Una vez añadido el código de nuestra clase `Scene`, entre el código de las clases `Snake` y `SnakeGame`, añadimos a continuación
+de aquella el código de nuestras primeras escenas:
+
+``` javascript
+class MainMenuScene extends Scene {
+  constructor(game) { super(game, SCENE_MAIN_MENU); }
+
+  paint(ctx) {
+    // Clean canvas
+    ctx.fillStyle = '#003300';
+    ctx.fillRect(0, 0, this.game.getWidth(), this.game.getHeight());
+
+    // Draw title
+    ctx.fillStyle = '#ffffff';
+    ctx.textAlign = 'center';
+    ctx.fillText('SNAKE', 150, 60);
+    ctx.fillText('Press Enter', 150, 90);
+  }
+
+  actions() {
+    // Load next scene
+    if (this.lastPress === KEY_ENTER) {
+      Scene.ChangeScene(SCENE_GAME);
+      this.lastPress = null;
+    }
+  }
+}
+
+class GameScene extends Scene {
+  constructor(game) {
+    super(game, SCENE_GAME);
+
+    this.movingDirection = MOVING_RIGHT;
+    this.pause = true;
+    this.score = 0;
+    this.gameover = false;
+
+    // Load assets
+    this.loadAssets();
+
+    // Create snake
+    this.snake = new Snake(300, 150, MOVING_RIGHT, 10, 10, '#00ff00', this.sectImg);
+
+    // Create initial food
+    this.food = new Food(450, 280, '#ff0000', this.foodImg, 10);
+  }
+
+  load() {
+    this.score = 0;
+    this.movingDirection = MOVING_RIGHT;
+
+    this.snake.reborn(300, 150, this.movingDirection);
+
+    this.food.relocate(this.game.getWidth(), this.game.getHeight());
+    this.gameover = false;
+  }
+
+  isPaused() { return this.pause ; }
+
+  isGameover() { return this.gameover ; }
+
+  loadAssets() {
+    let audioFormat = ((new Audio()).canPlayType('audio/ogg') != '') ? 0 : 1;
+
+    this.sectImg = new Image();
+    this.foodImg = new Image();
+    this.sectImg.src = ASSET_SNAKE_SECTION;
+    this.foodImg.src = ASSET_FOOD;
+
+    this.eat = new Audio(ASSET_EAT[audioFormat]);
+    this.over = new Audio(ASSET_OVER[audioFormat]);
+  }
+
+  paint(ctx) {
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(0, 0, this.game.getWidth(), this.game.getHeight());
+
+    // Draw score
+    ctx.fillStyle = '#ffffff';
+    ctx.fillText('Score: ' + this.score, 0, 20);
+
+    // Draw snake
+    this.snake.draw(ctx);
+
+    this.food.draw(ctx);
+
+    // Draw pause or gameover caption
+    if (this.isPaused()) {
+        ctx.textAlign = 'center';
+        if (this.isGameover()) {
+            ctx.fillText('GAME OVER', this.game.getWidth() / 2, this.game.getHeight() / 2);
+        } else {
+            ctx.fillText('PAUSE', this.game.getWidth() / 2, this.game.getHeight() / 2);
+        }
+        ctx.textAlign = 'left';
+    }
+  }
+
+  actions() {
+    if (!this.isPaused()) {
+      // GameOver reset
+      if (this.isGameover()) {
+        Scene.ChangeScene(SCENE_MAIN_MENU);
+      }
+
+      // Check direction
+      switch (this.lastPressed) {
+        case KEY_LEFT: this.movingDirection = MOVING_LEFT; break;
+        case KEY_UP: this.movingDirection = MOVING_UP; break;
+        case KEY_RIGHT: this.movingDirection = MOVING_RIGHT; break;
+        case KEY_DOWN: this.movingDirection = MOVING_DOWN; break;
+      }
+
+      // Move snake
+      this.snake.move(10, this.movingDirection, this.game.getWidth(), this.game.getHeight());
+
+      // Body Intersects
+      if (this.snake.hasBitten()) {
+          this.over.play();
+          this.gameover = true;
+          this.pause = true;
+      }
+
+      // Food eaten
+      if (this.snake.hasEaten(this.food)) {
+        this.eat.play();
+        this.score += 1; // also score++ or score = score + 1
+        this.food.relocate(this.game.getWidth(), this.game.getHeight());
+      }
+    }
+
+    // Pause/Unpause
+    if (this.lastPressed == KEY_ENTER) {
+        this.pause = !this.pause;
+        this.lastPressed = null;
+    }
+  }
+}
+```
+
+La escena del menú principal es muy sencilla: sobreescribe el constructor para invocar al de la clase `Scene` pasándole su nombre,
+no tiene método `load`, el método `paint` escribe texto y `actions` cambia a la escena del juego.
+
+La escena del juego es bastante más complicada:
+
+- Su constructor, tras llamar al constructor de la clase `Scene`, incluye todo el código del constructor de `SnakeGame` excepto
+los atributos del lienzo y la última tecla pulsada, que deben quedarse en esa clase para estar disponibles en todas las escenas.
+- Incluye los métodos `isPaused`, `isGameover` y `loadAssets` de `SnakeGame`.
+- El método `load` corresponde con el método `reset`, y `paint` y `actions` con sus homónimos de la clase `SnakeGame`.
+
+En ambas clases hay que usar la referencia a la instancia del juego para acceder a las dimensiones del lienzo.
+
+Por ultimo debemos modificar la clase `SnakeGame` para que en su constructor cree los dos nuevos objetos y cambia a la escena inicial.
+
+## Código final
+
+<div class="game_example">
+  <script type="application/javascript" src="assets/game-09.js"></script>
+  <canvas id="canvas" width="700" height="350" style="background:#999">[Canvas not supported by your browser.]</canvas>
+</div>
+<div>&nbsp;</div>
+
+``` javascript
+{% include_relative assets/game-09.js %}
+```
